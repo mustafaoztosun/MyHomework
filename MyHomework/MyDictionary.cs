91d4241ff6af453f97387e491bae53e9ce641008
16 Ocak Ders Sonu <mustafaoztosun.g@gmail.com>
2021-02-19 17:43:19 +0300

    Add project files.

---
+++ MyHomework/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey, TVal>
+    {
+        TKey[] keys;
+        TVal[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TVal[0];
+        }
+        public void Add(TKey t, TVal v)
+        {
+            TKey[] tempArrayKey = keys;
+            TVal[] tempArrayValue = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TVal[values.Length + 1];
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                keys[i] = tempArrayKey[i];
+            }
+            for (int i = 0; i < tempArrayValue.Length; i++)
+            {
+                values[i] = tempArrayValue[i];
+            }
+
+            keys[keys.Length - 1] = t;
+            values[values.Length - 1] = v;
+
+            Console.WriteLine("Araç Markası : " + t + " : " + "Fiyat : " + v);
+        }
+
+        public TKey[] itemkey
+        {
+            get { return keys; }
+        }
+        public TVal[] itemvalue
+        {
+            get { return values; }
+        }
+        public int Lenght
+        {
+            get { return keys.Length; }
+        }
+    }
+}
---
+++ MyHomework/Program.cs
@@ -0,0 +1,29 @@
+﻿using Dictionary;
+using System;
+using System.Collections.Generic;
+
+namespace MyHomework
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyDictionary<string, int> cars = new MyDictionary<string, int>();
+            cars.Add("Mercedes", 512000);
+            cars.Add("Audi", 25000);
+            cars.Add("Bmw", 32000);
+
+            Console.WriteLine(cars.Lenght);
+
+
+            foreach (var car in cars.itemkey)
+            {
+                Console.WriteLine(car);
+            }
+            foreach (var fiyat  in cars.itemvalue)
+            {
+                Console.WriteLine(fiyat);
+            }
+        }
+    }
+}
